4e0d4703b2f77197492cf23219c391ada366971f
aaron2024 <xieaaron@hotmail.com>
2021-03-06 15:42:04 -0800

    numerate anon class and id names

--- processing/tag_anon.py
+++ processing/tag_anon.py
@@ -20,13 +20,51 @@ with open(os.path.join(__location__,__target_file__), 'r', encoding="ISO-8859-1"
 
 soup = BeautifulSoup(target_html, 'html.parser')
 
+cur_classes = {
+    #'orgName': 'newName' 
+    }
+class_counter = 1
+id_counter = 1
+
 def tag_removed(soup):
     '''
-    change inline html text content so that it says 'ANON_TEXT' (incl. class names, type, style attributes etc.)
+    manipulate html tag names
     '''
+
     for tag in soup.findAll(True):
-        tag.attrs = tag.attrs.fromkeys(tag.attrs,'ANON_TEXT')
+        # tag.attrs = tag.attrs.fromkeys(tag.attrs,"")
+        tag.attrs = _tag_removed_helper(tag.attrs)
     return soup
+
+
+
+def _tag_removed_helper(attrs):
+    '''
+    Class need to check if already assigned a number, then replace with that
+    Id can only be used once in html, so just a simple counter to give names
+    '''
+    global cur_classes
+    global class_counter
+    global id_counter
+
+    for attr_type, attr_values in attrs.items():
+        if attr_type == 'class':
+            for i, class_name in enumerate(attr_values):
+                if class_name in cur_classes:
+                    attr_values[i] = cur_classes[class_name]
+                else:
+                    new_class_name = 'Class_' + str(class_counter)
+                    attr_values[i] = new_class_name
+                    cur_classes[class_name] = new_class_name
+                    class_counter += 1
+        elif attr_type == "id":
+            new_id_name = 'Id_' + str(id_counter)
+            attrs[attr_type] = new_id_name
+            id_counter += 1
+        else:
+            attrs[attr_type] = ""
+
+    return attrs
 
 def script_removed(soup):
     '''
