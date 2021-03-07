70cbd2315a2045204161708d039c659cbc0495c0
Alan <awguo2019@gmail.com>
2021-03-07 16:44:25 -0500

    updated nouns list + 1320 html files

--- webscraping-o/webscraping.py
+++ webscraping-o/webscraping.py
@@ -7,43 +7,43 @@ Created on Tue Mar  2 12:44:50 2021
 from bs4 import BeautifulSoup
 import requests
 
-#change this path to where the website list is saved
-a_file = open("C:/Users/Alan/Desktop/p-web/websitelist.txt")
+
+a_file = open("./websitelist.txt")
 file_contents = a_file.read()
 contents_split = file_contents.splitlines()
 
-updatelist = []
-title = []
+updatelist = [];
+title = [];
 for w in contents_split:
-    updatelist.append("https://www." + w)
+    updatelist.append("https://www." + w + ".com");
     title.append(w.split('.')[0])
-print(updatelist)
-print(title)
+print(updatelist);
+print(title);
 
-k = 0
+k = 0;
 
 for w in updatelist:   
-    print(w)
+    print(w);
     #grab the soup output for each website
     try: # need to open with try
         r = requests.get(w)
         soup = BeautifulSoup(r.content, features="lxml")
     except:
         #display faulty websites
-        #print(w)
-        k = k + 1
+        #print(w);
+        k = k + 1;
         continue
     
     #check where we are in program
-    print(k)
+    print(k);
     #make it more readable
-    html = soup.prettify()
+    html = soup.prettify();
     
     #write html data into folder
-    path = "C:/Users/Alan/Desktop/p-web/htmlfiles/" + title[k] + ".txt"
+    path = "./htmlfiles/" + title[k] + ".txt";
     
     #iterate name variable
-    k = k + 1
+    k = k + 1;
     
     #output into file
     with open(path,"w") as out:
