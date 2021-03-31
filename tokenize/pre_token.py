2066ebac99586322eb9b04d79f3be985e50b4080
aaron2024 <aaron.xie2024@gmail.com>
2021-03-30 22:48:27 -0700

    10k pre-tokenized files

--- tokenize/pre_token.py
+++ tokenize/pre_token.py
@@ -12,45 +12,45 @@ from spacy import util
 import os
 import re
 
-nlp = spacy.load("en_core_web_sm", disable=["tok2vec","tagger", "attribute_ruler", "lemmatizer"])
-
-
-# * Ditioncary of Special Cases: none
-special_cases = {}
-
-# * prefix_search, preceding punctuation: <
-prefixes = nlp.Defaults.prefixes
-prefixes = list(prefixes)
-prefixes.remove("<")
-prefixes.remove('"')
-prefixes.remove('—')
-prefixes.remove('–')
-prefix_search = (util.compile_prefix_regex(prefixes).search)
-
-# * suffix_search, succeeding punctuation: >
-suffixes = nlp.Defaults.suffixes
-suffixes = list(suffixes)
-suffixes.remove(">")
-suffixes.remove('"')
-suffix_search = (util.compile_suffix_regex(suffixes).search)
-
-# * infixes_finditer, non-whitespace separators: \n ="
-infix_re = re.compile('\n|=""|="')
-
-
-# * token_match, always stay together
-# token_match = re.compile('[A-z]+\/[A-z]+|[A-z]+-[A-z]+').search
-
-def custom_tokenizer(nlp):
-    return Tokenizer(nlp.vocab, rules=special_cases,
-                                prefix_search=prefix_search,
-                                suffix_search=suffix_search,
-                                infix_finditer=infix_re.finditer)
-
-nlp.tokenizer = custom_tokenizer(nlp)
-
 
 def perFile(sourceFile): 
+
+    nlp = spacy.load("en_core_web_sm", disable=["tok2vec","tagger", "attribute_ruler", "lemmatizer", "ner", "parser"])
+    nlp.max_length = 123456789 # disabled RAM heavy features so should be okay; but still be careful when running
+
+    # * Ditioncary of Special Cases: none
+    special_cases = {}
+
+    # * prefix_search, preceding punctuation: <
+    prefixes = nlp.Defaults.prefixes
+    prefixes = list(prefixes)
+    prefixes.remove("<")
+    prefixes.remove('"')
+    prefixes.remove('—')
+    prefixes.remove('–')
+    prefix_search = (util.compile_prefix_regex(prefixes).search)
+
+    # * suffix_search, succeeding punctuation: >
+    suffixes = nlp.Defaults.suffixes
+    suffixes = list(suffixes)
+    suffixes.remove(">")
+    suffixes.remove('"')
+    suffix_search = (util.compile_suffix_regex(suffixes).search)
+
+    # * infixes_finditer, non-whitespace separators: \n ="
+    infix_re = re.compile('\n|=""|="')
+
+
+    # * token_match, always stay together
+    # token_match = re.compile('[A-z]+\/[A-z]+|[A-z]+-[A-z]+').search
+
+    def custom_tokenizer(nlp):
+        return Tokenizer(nlp.vocab, rules=special_cases,
+                                    prefix_search=prefix_search,
+                                    suffix_search=suffix_search,
+                                    infix_finditer=infix_re.finditer)
+
+    nlp.tokenizer = custom_tokenizer(nlp)
 
     with open(sourceFile, 'r', encoding="ISO-8859-1") as f:
         try:
