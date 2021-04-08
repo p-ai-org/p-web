812f5049f7a006d128600621f2756a3b7f8732c1
Alan <awguo2019@gmail.com>
2021-04-08 02:51:03 -0400

    gpt2

---
+++ gpt-2/mergehtmltext.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 20:58:55 2021
+
+@author: Alan
+"""
+
+import glob
+
+
+read_files = glob.glob("./smaller_sample/*.txt")
+
+with open("result.txt", "w", encoding="utf8") as outfile:
+    for f in read_files:
+        with open(f, "r") as infile:
+            try:
+                print(f);
+                outfile.write(infile.read())
+                outfile.write("\n")
+                outfile.write("\n")
+                outfile.write("<|endoftext|>");
+                outfile.write("\n")
+                outfile.write("\n")
+            except:
+                print("error");
