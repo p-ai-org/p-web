44b394c55d23f00f26be6a9a018e74e43d3b51c5
Aaron Xie <aaron.xie2024@gmail.com>
2021-05-01 14:45:07 -0700

    class and id name -less processed files

--- processing/html_processed.py
+++ processing/html_processed.py
@@ -31,30 +31,32 @@ def tag_removed(soup, cur_classes, class_counter, id_counter, cur_ids):
     for tag in soup.findAll(True):
         for attr_type, attr_values in tag.attrs.items():
             
-            # class subbed with placeholder
-            if attr_type == 'class':
-                for i, class_name in enumerate(attr_values):
-                    if class_name in cur_classes:
-                        attr_values[i] = cur_classes[class_name]
-                    else:
-                        new_class_name = 'class_' + str(class_counter)
-                        attr_values[i] = new_class_name
-                        cur_classes[class_name] = new_class_name
-                        class_counter += 1
+            # NOTE remove class name replacement and id replacement
+            # class subbed with placeholder 
+            # if attr_type == 'class':
+            #     for i, class_name in enumerate(attr_values):
+            #         if class_name in cur_classes:
+            #             attr_values[i] = cur_classes[class_name]
+            #         else:
+            #             new_class_name = 'class_' + str(class_counter)
+            #             attr_values[i] = new_class_name
+            #             cur_classes[class_name] = new_class_name
+            #             class_counter += 1
+
             # id subbed with placeholder
-            elif attr_type == "id":
-                if attr_values in cur_ids:
-                    tag.attrs[attr_type] = cur_ids[attr_values] 
-                else:
-                    new_id_name = 'id_' + str(id_counter)
-                    cur_ids[attr_values] = new_id_name
-                    tag.attrs[attr_type] = new_id_name
-                    id_counter += 1
+            # elif attr_type == "id":
+            #     if attr_values in cur_ids:
+            #         tag.attrs[attr_type] = cur_ids[attr_values] 
+            #     else:
+            #         new_id_name = 'id_' + str(id_counter)
+            #         cur_ids[attr_values] = new_id_name
+            #         tag.attrs[attr_type] = new_id_name
+            #         id_counter += 1
             
             # aria tags to replace 
 
             # aria-labeledby is reference to ID so replace with ID
-            elif re.search("aria-labelledby", attr_type) is not None or re.search("aria-describedby", attr_type) is not None:
+            if re.search("aria-labelledby", attr_type) is not None or re.search("aria-describedby", attr_type) is not None:
                 if attr_values in cur_ids:
                     tag.attrs[attr_type] = cur_ids[attr_values]
                 else:
