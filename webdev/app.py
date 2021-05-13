17b112a52512dde4d6c58c5f5ca8159c2e34e57f
al1729 <andyliu1708@gmail.com>
2021-05-13 15:23:22 -0700

    streamlit bugfixes

--- webdev/app.py
+++ webdev/app.py
@@ -7,6 +7,7 @@ import json
 import numpy as np
 
 from bs4 import BeautifulSoup
+import random
 import streamlit as st
 
 tf.reset_default_graph()
@@ -17,31 +18,32 @@ modelpath = "/Users/andyliu/Downloads/checkpoint/355_10mb"
 
 #@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, model_name = modelpath)
-
-
-def complete(pre, ml):
-    output = gpt2.generate(sess, include_prefix=True, prefix=pre, length=ml, model_name = modelpath, return_as_list=True)[0]
+def complete(pre, maxlength, classes):
+    output = gpt2.generate(sess, include_prefix=True, prefix=pre, length=maxlength, model_name = modelpath, return_as_list=True)[0]
     html = pre + output
-    #you would add in classes here
     soup = BeautifulSoup(html)
+    if len(classes) > 0 and soup.find("div") is not None:
+        for ele in soup.find("div"):
+            ele['class'] = random.choice(classes)
     processed = soup.prettify()
     return(processed)
 
 textbox = st.text_area('Start your HTML:', '', height=200, max_chars=256)
+classes = st.text_area('Classes (optional, separated by commas):', '', height=50, max_chars = 128)
+slider = st.slider('Max text length (in tokens)', 1, 128)
 button = st.button('Generate')
 
-slider = st.slider('Max text length (in tokens)', 16, 512)
-
-st.subheader("Generated HTML Template:")
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, model_name = modelpath)
 
 if button:
-    output_text = complete(textbox, slider)
+    st.subheader("Generated HTML Template:")
+    class_list = classes.split(',')
+    output_text = complete(textbox, slider, class_list)
     for line in output_text.split('\n'):
         st.markdown(line)
 
-st.button('Download')
+    st.button('Download')
 
 
 
